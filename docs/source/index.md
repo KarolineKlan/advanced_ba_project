0212c0989d2a2d09e1d99d06b595cc32f310099e
KarolineKlan <karolineklanhansen@gmail.com>
2025-03-05 11:34:36 +0100

    init cookiecutter project

---
+++ docs/source/index.md
@@ -0,0 +1,3 @@
+## Documentation
+
+Documentation for advanced_ba_project
---
+++ src/advanced_ba_project/train.py
@@ -0,0 +1,10 @@
+from advanced_ba_project.model import Model
+from advanced_ba_project.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
